894978f49705d84c67c9ebfa2cf21fce829154dd
pdiakumis <peterdiakumis@gmail.com>
2024-04-14 10:53:45 +1000

    refactor quarto report

--- inst/reports/seqrunsum/funcs.R
+++ inst/reports/seqrunsum/funcs.R
@@ -35,18 +35,18 @@ funcs <- list(
     clrs <- RColorBrewer::brewer.pal.info |>
       tibble::rownames_to_column(var = "name") |>
       tibble::as_tibble() |>
-      dplyr::filter(name %in% c("Set3", "Set2", "Pastel2", "Pastel1")) |>
-      dplyr::select(name, maxcolors) |>
-      dplyr::arrange(dplyr::desc(name)) |>
-      dplyr::rowwise() |>
-      dplyr::mutate(
+      filter(name %in% c("Set3", "Set2", "Pastel2", "Pastel1")) |>
+      select(name, maxcolors) |>
+      arrange(desc(name)) |>
+      rowwise() |>
+      mutate(
         clrs = list(RColorBrewer::brewer.pal(n = .data$maxcolors, name = .data$name))
       ) |>
-      dplyr::ungroup() |>
+      ungroup() |>
       tidyr::unnest(clrs) |>
-      dplyr::pull(clrs)
+      pull(clrs)
     max_col <- length(clrs)
-    assertthat::assert_that(nc <= max_col, nc > 0, msg = glue("Number of colours should be a positive number less than {max_col + 1}!"))
+    stopifnot(nc <= max_col, nc > 0)
     clrs[seq_len(nc)]
   },
   #----#
@@ -63,11 +63,11 @@ funcs <- list(
   #----#
   add_totals = function(x) {
     x |>
-      dplyr::ungroup() %>%
-      dplyr::bind_rows(
-        dplyr::summarise(
-          ., dplyr::across(dplyr::where(is.numeric), sum),
-          dplyr::across(dplyr::where(is.character), ~"Total")
+      ungroup() %>%
+      bind_rows(
+        summarise(
+          ., across(where(is.numeric), sum),
+          across(where(is.character), ~"Total")
         )
       )
   },
@@ -92,5 +92,55 @@ funcs <- list(
           )
         )
       )
+  },
+  #----#
+  get_ids = function(pmeta_tidy, id) {
+    .get_ids <- function(tbl, id) {
+      tbl |>
+        select(contains(id)) |>
+        unlist() |>
+        unique()
+    }
+    pmeta_tidy |>
+      purrr::map(.get_ids, {{ id }}) |>
+      unlist() |>
+      unique() |>
+      sort()
+  },
+  #----#
+  plot_vistime = function(pmeta_sumy) {
+    p1 <- pmeta_sumy |>
+      arrange(sbj_lib, type_name) |>
+      group_by(sbj_lib, type_name) |>
+      mutate(
+        n_type = n(),
+        is_dup_type = n_type > 1,
+      ) |>
+      ungroup() |>
+      # take care of rnasum re-runs
+      mutate(sbj_lib = if_else(is_dup_type, paste0(sbj_lib, "_", portal_run_id), sbj_lib)) |>
+      left_join(clrs1, by = "sbjid") |>
+      mutate(sbjid = factor(.data$sbjid, levels = clrs1$sbjid))
+    vistime1 <- p1 |>
+      vistime::vistime(
+        col.event = "sbj_lib",
+        col.group = "type_name",
+        col.color = "color",
+        show_labels = TRUE,
+        optimize_y = FALSE,
+        linewidth = 15
+      )
+
+    # Decrease font size
+    pp <- plotly::plotly_build(vistime1)
+    font_size <- 11
+    # text_pos <- "center"
+    for (i in seq_along(pp$x$data)) {
+      if (pp$x$data[[i]]$mode == "text") {
+        pp$x$data[[i]]$textfont$size <- font_size
+        # pp$x$data[[i]]$textposition <- text_pos
+      }
+    }
+    pp
   }
 )
